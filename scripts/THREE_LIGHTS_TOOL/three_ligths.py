73b7e82151238859c370e3a53b6c50f35168302f
kalle <kalleholmqvist@icloud.com>
2025-08-06 19:03:18 +0200

    Karl (2025-08-06): I have added a new python package that creates three spotlights. I now works. Yee!

---
+++ scripts/THREE_LIGHTS_TOOL/three_ligths.py
@@ -0,0 +1,41 @@
+import os
+import maya.cmds as cmds
+
+class Lights:
+
+    def __init__(self):
+        pass
+
+    def interface(self):
+        if cmds.window('threeLightsWin', exists=True):
+            cmds.deleteUI('threeLightsWin')
+
+        root_path = os.path.dirname(os.path.abspath(__file__))
+        ui_path = os.path.join(root_path, '3_lights_tool_interface.ui')
+
+        window = cmds.loadUI(f=ui_path)
+        cmds.renameUI(window, 'threeLightsWin')
+        cmds.showWindow('threeLightsWin')
+
+        cmds.button('three_lights_button', e=True, c=self.lights)
+
+    def lights(self, *args):
+        try:
+            rot_1 = float(cmds.textField('light_1_rot_line', q=True, text=True))
+            dist_1 = float(cmds.textField('light_1_dist_line', q=True, text=True))
+
+            rot_2 = float(cmds.textField('light_2_rot_line', q=True, text=True))
+            dist_2 = float(cmds.textField('light_2_dist_line', q=True, text=True))
+
+            rot_3 = float(cmds.textField('light_3_rot_line', q=True, text=True))
+            dist_3 = float(cmds.textField('light_3_dist_line', q=True, text=True))
+
+            for i, (rot, dist) in enumerate([(rot_1, dist_1), (rot_2, dist_2), (rot_3, dist_3)], start=1):
+                light = cmds.spotLight()
+                light_transform = cmds.listRelatives(light, parent=True)[0]
+                cmds.setAttr(f"{light_transform}.rotateY", rot)
+                cmds.setAttr(f"{light_transform}.translateZ", dist)
+
+        except ValueError:
+            cmds.warning('Enter valid numbers in all fields')
+
--- scripts/misc.py
+++ scripts/misc.py
@@ -2,6 +2,22 @@ import maya.cmds as cmds
 
 #cmds.pointLight(rot=(90,0,10))
 
+window = cmds.window(title='Tool rool')
+
+# Layouts
+main_layout = cmds.rowColumnLayout(numberOfColumns=2, columnAttach=(1, 'right', 0), columnWidth=[(1, 100), (2, 250)])
+side_row = cmds.rowLayout(adj=True, numberOfColumns=2, parent=main_layout)
+
+# Create a text field
+text_field = cmds.textField(parent=side_row)
+
+
+cmds.showWindow(window)
+
+
+
+'''
 cmds.spotLight(pos=(0.3,0,0), rot=(0,90,0))
 
-cmds.spotLight(pos=(-0.2,0,0), rot=(0,-90,0))
+#cmds.spotLight(pos=(-0.2,0,0), rot=(0,-90,0))
+'''
